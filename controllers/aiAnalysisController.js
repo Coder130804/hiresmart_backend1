0b72bbbd794663e5b4c6d3c39d570434d08c797d
Coder130804 <arpitasom157@yahoo.com>
2025-07-31 00:05:40 +0530

    Fix: Added tmp-promise

--- controllers/aiAnalysisController.js
+++ controllers/aiAnalysisController.js
@@ -6,19 +6,19 @@ const Interview = require('../models/Interview');
 const InterviewSession = require('../models/InterviewSession');
 
 // ✅ Import ALL question files
-const { drcrQuestions } = require('../../shared/questions/drcr');
-const { eduQuestions } = require('../../shared/questions/edu');
-const { genderQuestions } = require('../../shared/questions/gender');
-const { hnQuestions } = require('../../shared/questions/hn');
-const { hrQuestions } = require('../../shared/questions/hr');
-const { infraQuestions } = require('../../shared/questions/infra');
-const { lsdQuestions } = require('../../shared/questions/lsd');
-const { merQuestions } = require('../../shared/questions/mer');
-const { mlrQuestions } = require('../../shared/questions/mlr');
-const { tribalQuestions } = require('../../shared/questions/tribal');
-const { volunteerQuestions } = require('../../shared/questions/volunteer');
-const { washQuestions } = require('../../shared/questions/wash');
-const { youthQuestions } = require('../../shared/questions/youth');
+const { drcrQuestions } = require('../../shared/questions/drcr.js');
+const { eduQuestions } = require('../../shared/questions/edu.js');
+const { genderQuestions } = require('../../shared/questions/gender.js');
+const { hnQuestions } = require('../../shared/questions/hn.js');
+const { hrQuestions } = require('../../shared/questions/hr.js');
+const { infraQuestions } = require('../../shared/questions/infra.js');
+const { lsdQuestions } = require('../../shared/questions/lsd.js');
+const { merQuestions } = require('../../shared/questions/mer.js');
+const { mlrQuestions } = require('../../shared/questions/mlr.js');
+const { tribalQuestions } = require('../../shared/questions/tribal.js');
+const { volunteerQuestions } = require('../../shared/questions/volunteer.js');
+const { washQuestions } = require('../../shared/questions/wash.js');
+const { youthQuestions } = require('../../shared/questions/youth.js');
 
 // ✅ Combine all questions and flatten
 const allQuestionSets = [
